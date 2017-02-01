5ef3082ec6101459de7a30d6841c25ba65e52dbe
David Wybourn <david.wybourn@gmail.com>
2017-02-01 20:22:10 +0000

    Initial commit

---
+++ src/main/java/wybourn/david/TaskDemoApplication.java
@@ -0,0 +1,12 @@
+package wybourn.david;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskDemoApplication.class, args);
+	}
+}
